7b09230d16f27598f5f4224836d75ef093955dba
OpenOLPart <74041190+OpenOLPart@users.noreply.github.com>
2022-08-21 01:53:04 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # OLPart: Online Learning based Resource Partitioning for Colocating Multiple Latency-Critical Jobs on Commodity Computers
 OLPart is tested on a CentOS 7.8 Server with Linux 4.1.0 using Python3.7. Please install the following library depencies for running OLPart.
 
-# Dependencies
-```
-pip3 install numpy   
-apt-get install intel-cmt-cat
-```
-
-OLPart uses the Linux _perf_ to collect runtime status of each job for guiding the quick and smart exploration. 
-Please ensure that Intel CAT, MBA, and taskset tools are supported and active in your system.
-Click this link to confirm: https://github.com/intel/intel-cmt-cat.
 
 # The benchmark suites evaluated in Orchid
 
@@ -21,11 +12,60 @@ PARSEC 3.0: https://parsec.cs.princeton.edu/parsec3-doc.htm
 
 ## File Description
 ```
-get_arm.py : generate resource configurations as arms.
-get_config.py : some tool functions.
-OLUCB.py: main algorithm of OLPart.
-vote_bandit.py : main file used to make online resource partitioning decisions.
+├─config: the config files and running commands for all evaluated jobs
+├─environment: some environment setting
+|   ├─env.sh: environment setting for running OLPart on the server at first time
+|   ├─rerun_docker.py: after each job has been instantiated in a separate container, 
+|   |                   restart the server and use the code required for those jobs。
+|   |─Dockerfile: the docker file to build containers for Dockerfile
+|   |─correct_config.sh: the auxiliary file for docker fil
+├─get_arm.py: generate resource configurations as arms.
+├─run_and_get_config.py : some tool functions.
+├─OLUCB.py: main algorithm of OLPart.
+├─vote_bandit.py : main file used to make online resource partitioning decisions.
 ```
 
-## run program:
-    python vote_bandit.py
+## Run OLPart:
+### Install Dependencies
+```
+pip3 install numpy   
+sudo yum install intel-cmt-cat
+sudo yum install msr-tools
+sudo yum install docker
+```
+OLPart uses the Linux _perf_ to collect runtime status of each job for guiding the quick and smart exploration. 
+Please ensure that Intel CAT, MBA, and taskset tools are supported and active in your system.
+Click this link to confirm: https://github.com/intel/intel-cmt-cat.
+
+### Instantiate jobs in the docker
+Each job is instantiated in a separate container, we use _docker_ to create containers.
+Please run _Dockerfile_ to create the containers. 
+ 
+Then we record all evaluated jobs' docker ppid (as shown as _APP_DOCKER_PPID_ in file _run_and_get_config.py_ ).
+
+```
+docker top [container id]
+```
+
+Then we add config files to each evaluated job, the config files and running commands are in the directory _config_. 
+Please copy the _sh_ and _py_ files to corrspond job's container.
+
+Noted that we should set up the mapping address between the host and each docker to get latency information as the function  
+_get_LC_app_latency_and_judge_ in file _run_and_get_config.py_.
+
+docker run -t -d --name=[Name] -v [the address in host directory]:[the address in docker] [IMAGE], like:
+
+```
+docker run -t -d --name=img-dnn -v /home/crb/bandit_clite/share_data:/tmp/share bandit_last:v1
+```
+
+### Run
+If you are running OLPart on the server for the first time, please run:
+```
+run ./env.sh
+```
+Then run the main file:
+```
+python vote_bandit.py
+```
+The functionality of the Class and Function involved in all Python files is shown in the comments in the codes.
--- vote_bandit.py
+++ vote_bandit.py
@@ -8,18 +8,23 @@ import numpy as np
 
 
 from get_arm import bin_search, get_llc_bandwith_config, list_duplicates
-from get_config import gen_config, gen_init_config, LC_APP_NAMES, get_now_ipc
+from run_and_get_config import gen_config, gen_init_config, LC_APP_NAMES, get_now_ipc
 from OLUCB import OLLinUCB
 
 
 
-def train_success(rounds=30):
-    nof_counters = 22
-    nof_colocation = len(colocation_list)
+def train_success(nof_counters,colocation_list,alpha,rounds):
+    """
+    :param nof_counters: Number of selected performance counters for providing contexts
+    :param colocation_list: The coming colocations
+    :param alpha:  Explore-exploit parameter
+    :param rounds: The Allowed Sampling Times For a Colocation
+    :return:
+    """
 
     init_alg = "fair"
 
-    alpha = 0.01
+    nof_colocation = len(colocation_list)
     # number of bandit versions
     mab_num = 3
     mab_1 = OLLinUCB(nof_counters, alpha, colocation_list[0])
@@ -27,22 +32,30 @@ def train_success(rounds=30):
     mab_list = [mab_1, mab_2, mab_3]
     mab_count = 1
     tmp_cumulative_reward, tmp_G = [[] for _ in range(mab_num)], [0 for _ in range(mab_num)]
-    nrounds = 0
-    for col_items in range(nof_colocation):
-        app_id = colocation_list[col_items]
 
-        lc_app, bg_app = [], []
-        for i in app_id:
+
+
+    for col_items in range(nof_colocation):
+        job_id = colocation_list[col_items]
+
+        lc_job, be_job = [], []
+        for i in job_id:
             if i in LC_APP_NAMES:
-                lc_app.append(i)
+                lc_job.append(i)
             else:
-                bg_app.append(i)
+                be_job.append(i)
+
+
 
         chose_arm_storage = []
         reward_arms = []
 
-        core_list, llc_config, mb_config, chosen_arms = gen_init_config(app_id, llc_arm_orders,
+        # Offer the equally partitioning scheme in the initial
+        core_list, llc_config, mb_config, chosen_arms = gen_init_config(job_id, llc_arm_orders,
                                                                         alg=init_alg)
+
+        # run the colocations in order
+        run_be(be_job,core_list[len(lc_app):])
 
         for i in range(rounds):
             if nrounds % 60 == 0:
@@ -55,9 +68,9 @@ def train_success(rounds=30):
 
             nrounds += 1
             if i == 0:
-                context, another_context, reward, p95_list = get_now_ipc(lc_app, bg_app,
+                context, another_context, reward, p95_list = get_now_ipc(lc_job, be_job,
                                                                          performamce_counters)
-                mab_1.add_del_app(app_id)
+                mab_1.add_del_job(job_id)
 
                 chose_arm_storage.append([core_list, llc_config, mb_config])
                 reward_arms, chosen_arms, tmp_cumulative_reward[0], tmp_G[0] = onlineEvaluate(mab_1, reward,
@@ -71,7 +84,7 @@ def train_success(rounds=30):
                 tmp_chosen_arms = []
                 for ii in range(len(mab_list)):
                     if mab_list[ii] != 0:
-                        mab_list[ii].add_del_app(app_id)
+                        mab_list[ii].add_del_job(job_id)
                         reward_arms_1, chosen_arms_1, tmp_cumulative_reward[ii], tmp_G[ii] = onlineEvaluate(
                             mab_list[ii], reward,
                             reward_arms, chosen_arms,
@@ -81,15 +94,18 @@ def train_success(rounds=30):
                             tmp_G[ii], i)
                         tmp_chosen_arms.append(chosen_arms_1)
 
-                chosen_arms = list_duplicates(tmp_chosen_arms, app_id)
+                chosen_arms = list_duplicates(tmp_chosen_arms, job_id)
                 reward_arms = reward_arms_1
 
-                core_list, llc_config, mb_config = gen_config(app_id, chosen_arms, llc_arm_orders,
+                core_list, llc_config, mb_config = gen_config(job_id, chosen_arms, llc_arm_orders,
                                                               mb_arm_orders)
                 time.sleep(1)
-                context, another_context, reward, p95_list = get_now_ipc(lc_app, bg_app,
+                context, another_context, reward, p95_list = get_now_ipc(lc_job, be_job,
                                                                          performamce_counters)
                 chose_arm_storage.append([core_list, llc_config, mb_config])
+
+
+        stop_the_current_colocation()
 
         best_reward_id = np.argmax(reward_arms)
         best_config = chose_arm_storage[best_reward_id - 1]
@@ -106,7 +122,6 @@ def onlineEvaluate(mab, reward, reward_arms, chosen_arms, cumulative_reward, con
     :param mab:
     :param rewards: ipc/delay
     :param contexts: counter
-    :param nrounds:
     :return:
     """
 
@@ -126,7 +141,15 @@ def onlineEvaluate(mab, reward, reward_arms, chosen_arms, cumulative_reward, con
 
 if __name__ == "__main__":
     llc_arm_orders, mb_arm_orders = get_llc_bandwith_config()
+    # In the real sysytem, we identify the coming colocation  and get the PID of each job.
+    # In the simulation experiment, we list the coming colocations in order, like the following example.
     colocation_list = [['img-dnn', 'xapian', "masstree"]]
+    # In the simulation experiment, we list the loads for the coming colocations in order, like the following example.
+    load_list = [[3,4,2]]
+
     # The performance counters we selected as shown in the paper
     performamce_counters = we_choose()
-    train_success()
+    nof_counters = len(performamce_counters)
+
+    alpha = 0.01
+    train_success(nof_counters=nof_counters,colocation_list=colocation_list,alpha=0.01,rounds=30)
